ffc563500eea1ce3c97cd5c2dbb307e33c835e7f
Grégory Copin <gcopin@inouit.com>
2014-04-02 11:39:19 +0200

    [fix] dirs in imageCaption

--- app/templates/Configuration/TSconfig/_imageCaption.ts
+++ app/templates/Configuration/TSconfig/_imageCaption.ts
@@ -1,9 +1,9 @@
 tx_gridelements.setup.<%= params.slugifiedContentName %> {
 
-  title = LLL:EXT:<%= currentDir %>/<%= dirs.llDir %>/locallang_db.xlf:TSconfig.<%= params.slugifiedContentName %>.title
-  description = LLL:EXT:<%= currentDir %>/<%= dirs.llDir %>/locallang_db.xlf:TSconfig.<%= params.slugifiedContentName %>.description
-  flexformDS = FILE:EXT:<%= currentDir %>/<%= dirs.flexFormDir %>/<%= params.slugifiedContentName %>.xml
-  icon = EXT:<%= currentDir %>/<%= dirs.iconsDir %>/<%= params.slugifiedContentName %>.gif
+  title = LLL:EXT:<%= currentDir %>/<%= dirs.llDir %>locallang_db.xlf:TSconfig.<%= params.slugifiedContentName %>.title
+  description = LLL:EXT:<%= currentDir %>/<%= dirs.llDir %>locallang_db.xlf:TSconfig.<%= params.slugifiedContentName %>.description
+  flexformDS = FILE:EXT:<%= currentDir %>/<%= dirs.flexFormDir %><%= params.slugifiedContentName %>.xml
+  icon = EXT:<%= currentDir %>/<%= dirs.iconsDir %><%= params.slugifiedContentName %>.gif
 
   config {
     # field's name wich will be displayed
--- app/templates/Configuration/Typoscript/_imageCaption.ts
+++ app/templates/Configuration/Typoscript/_imageCaption.ts
@@ -1,4 +1,4 @@
-page.includeCSS.<%= params.slugifiedContentName %> = EXT:<%= currentDir %>/<%= dirs.cssDir %>/<%= params.slugifiedContentName %>.css
+page.includeCSS.<%= params.slugifiedContentName %> = EXT:<%= currentDir %>/<%= dirs.cssDir %><%= params.slugifiedContentName %>.css
 
 tt_content.gridelements_pi1.20.10.setup.<%= params.slugifiedContentName %> {
   prepend = COA
