3050bd7e8cc7b5aaf233799e275ac8672fe1e294
Grégory Copin <gcopin@inouit.com>
2014-05-02 18:07:26 +0200

    [fix] #4

--- app/templates/Configuration/Typoscript/_custom.ts
+++ app/templates/Configuration/Typoscript/_custom.ts
@@ -1,4 +1,5 @@
-page.includeCSS.<%= params.slugifiedContentName %> = EXT:<%= currentDir %>/<%= dirs.cssDir %><%= params.slugifiedContentName %>.css
+#page.includeCSS.<%= params.slugifiedContentName %> = EXT:<%= currentDir %>/<%= dirs.cssDir %><%= params.slugifiedContentName %>.css
+#page.includeJSFooter.<%= params.slugifiedContentName %> = EXT:<%= currentDir %>/<%= dirs.jsDir %><%= params.slugifiedContentName %>.js
 
 tt_content.gridelements_pi1.20.10.setup.<%= params.slugifiedContentName %> {
   prepend = COA
